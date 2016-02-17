8a70a89fbf07284a40cab2b5fa95168800514fbb
ILUXONCHIK ILUXA <iluxon4ik@hotmail.com>
2016-02-17 20:24:54 +0000

    Add some business login to Contact and Person.

--- phonebook/phonebook/src/main/java/pt/ist/phonebook/domain/Contact.java
+++ phonebook/phonebook/src/main/java/pt/ist/phonebook/domain/Contact.java
@@ -2,8 +2,15 @@ package pt.ist.phonebook.domain;
 
 public class Contact extends Contact_Base {
     
-    public Contact() {
-        super();
+    public Contact(String name, int phoneNumber) {
+        this.setName(name);
+        this.setPhoneNumber(phoneNumber);
     }
-    
+
+    @Override
+    public void setPerson(Person person) {
+        // NOTE: there is an actual relation (table) of Person - Contact,
+        // so this will add both of the entries, the current contact and the person
+        person.addContact(this);
+    }
 }
--- phonebook/phonebook/src/main/java/pt/ist/phonebook/domain/Person.java
+++ phonebook/phonebook/src/main/java/pt/ist/phonebook/domain/Person.java
@@ -5,5 +5,16 @@ public class Person extends Person_Base {
     public Person() {
         super();
     }
+
+    @Override
+    public void addContact(Contact newContact) {
+        for (Contact c : getContactSet()) {
+            if (c.getName().equals(newContact.getName())) {
+                throw new IllegalArgumentException("A Person instance cannot " +
+                        "have two or more Contact instances with the same name.");
+            }
+        }
+        super.addContact(newContact);
+    }
     
 }
