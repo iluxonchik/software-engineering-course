e2743c43b7cbbbba5269741726abf264ecdcd261
ILUXONCHIK ILUXA <iluxon4ik@hotmail.com>
2016-02-18 12:59:10 +0000

    Implemented singleton pattern for PhoneBook.java

--- phonebook/phonebook/src/main/java/pt/ist/phonebook/domain/PhoneBook.java
+++ phonebook/phonebook/src/main/java/pt/ist/phonebook/domain/PhoneBook.java
@@ -1,9 +1,17 @@
 package pt.ist.phonebook.domain;
 
+import pt.ist.fenixframework.FenixFramework;
+
 public class PhoneBook extends PhoneBook_Base {
-    
-    public PhoneBook() {
-        super();
+
+    public static PhoneBook getInstance() {
+        PhoneBook pb = FenixFramework.getDomainRoot().getPhonebook();
+        if (pb == null)
+            pb = new PhoneBook();
+        return pb;
+    }
+    private PhoneBook() {
+        FenixFramework.getDomainRoot().setPhonebook(this);
     }
     
 }
